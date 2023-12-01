66aa36e4eb2dd2980db17005fbe2cf50a9b13095
kochurgen <kochurgen@gmail.com>
2023-12-02 00:11:19 +0200

    first push

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import pyaudio
+
+
+def print_hi():
+    # Use a breakpoint in the code line below to debug your script.
+    p = pyaudio.PyAudio()
+    for i in range(p.get_device_count()):
+        print(i, p.get_device_info_by_index(i)['name'])
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
